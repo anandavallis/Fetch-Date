c6b65a117549e7b6b07d8c590aeda37669e426a8
anandavallis <sanandavalli7@gmail.com>
2025-07-02 15:33:51 +0530

    Create App.jsx

---
+++ Fetch User Data/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserComponent from './UserComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>Fetch User Example</h1>
+      <UserComponent />
+    </div>
+  );
+}
+
+export default App;
